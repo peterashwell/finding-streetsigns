a72a3a901ee889efb6e39a922b1f01d925139b4b
peterashwell <peter.w.ashwell@gmail.com>
2015-05-26 09:45:32 +1000

    Refactor do_sift to use wrapper class
    Add results after running on riley dataset

---
+++ do.sh
@@ -0,0 +1,1 @@
+python2 do_sift.py riley_training/signs ../panoramas/riley_st/images
--- do_sift.py
+++ do_sift.py
@@ -7,7 +7,7 @@ from sift_wrapper import SiftWrapper
 
 MIN_MATCH_COUNT = 3
 # Lower - more specifity for matches
-THRESHOLD = 0.5
+THRESHOLD = 0.6
 
 training_path = sys.argv[1]
 query_path = sys.argv[2]
@@ -18,56 +18,39 @@ query_images = os.listdir(query_path)
 one_train_image = training_images[2]
 one_query_image = query_images[13]
 
-img1 = cv2.imread(
-    os.path.join(query_path, one_query_image), 0
-)
-img2 = cv2.imread(
-    os.path.join(training_path, one_train_image), 0
-)
-
 sw = SiftWrapper()
 
-sift_img1 = sw.do_sift(img1)
-sift_img2 = sw.do_sift(img2)
+for qnum, query_fname in enumerate(query_images):
+    print "reading {0}".format(query_fname)
+    training_hits = 0
+    query_image = cv2.imread(
+        os.path.join(query_path, query_fname), 0
+    )
+    output_image = np.copy(query_image)
 
-knn_result = sw.do_knn_sift(sift_img1, sift_img2)
+    sift_query = sw.do_sift(query_image)
+    for train_fname in training_images:
+        train_image = cv2.imread(
+            os.path.join(training_path, train_fname), 0
+        )
 
-if knn_result.num_found > MIN_MATCH_COUNT:
-    print knn_result.source_points
-    src_pts = np.float32(knn_result.source_points).reshape(-1, 1, 2)
-    dst_pts = np.float32(knn_result.destination_points).reshape(-1, 1, 2)
+        sift_train = sw.do_sift(train_image)
 
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-    matchesMask = mask.ravel().tolist()
+        knn_result = sw.do_knn_sift(sift_train, sift_query)
 
-    h, w = img1.shape
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv2.perspectiveTransform(pts,M)
+        if knn_result.num_found > MIN_MATCH_COUNT:
+            training_hits += 1
+            print "matched: {0}".format(train_fname)
+            src_pts = np.float32(knn_result.source_points).reshape(-1, 1, 2)
+            dst_pts = np.float32(knn_result.destination_points).reshape(
+                -1, 1, 2
+            )
 
-    #img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3)
-
-    for src_pt in src_pts:
-        print("src_pt:", src_pt)
-        cv2.circle(img1, tuple(src_pt[0]), 5, (255, 255, 255), -1)
-
-    for dst_pt in dst_pts:
-        print("dst_pt:", dst_pt)
-        cv2.circle(img2, tuple(dst_pt[0]), 5, (255, 255, 255), -1);
-
-else:
-    print("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
-    matchesMask = None
-
-
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
-
-#img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-print("writing 1")
-cv2.imwrite('queryout.png', img1)
-print("writing 2")
-cv2.imwrite('trainout.png', img2)
-
-#plt.imshow(img3, 'gray'),plt.show()
+            for dst_pt in dst_pts:
+                cv2.circle(
+                    output_image, tuple(dst_pt[0]), 5, (255, 255, 255), -1
+                )
+    if training_hits > 0:
+        # Write each query image out with markers from training images
+        output_path = os.path.join('results', 'riley', '{0}_query.png'.format(qnum))
+        cv2.imwrite(output_path, output_image)
