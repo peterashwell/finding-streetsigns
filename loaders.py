7733f46c6da896218746f8aca295d0fde4b27410
peterashwell <peter.w.ashwell@gmail.com>
2015-05-27 17:22:06 +1000

    Use image luminosity in loader (Lab L channel)

--- loaders.py
+++ loaders.py
@@ -1,14 +1,14 @@
 import cv2
 from skimage import color
+import numpy as np
 
 
 def open_lab_image(path):
     # The 1 tells openCV we want an RGB image
     rgb = cv2.imread(path, 1)
     lab_image = color.rgb2lab(rgb)
-    cv2.imwrite('l.jpg', lab_image[:, :, 0])
-    cv2.imwrite('a.jpg', lab_image[:, :, 1])
-    cv2.imwrite('b.jpg', lab_image[:, :, 2])
+    return (lab_image[:, :, 0] / 100 * 255).astype(np.uint8)
 
 if __name__ == '__main__':
-    open_lab_image('/home/peter/streetview/tmp/hsb_test.jpg')
+    luminosity = open_lab_image('/home/peter/streetview/tmp/hsb_test.jpg')
+    cv2.imwrite('test_luminosity.png', luminosity)
